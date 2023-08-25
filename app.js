836e8cc8d6a57bf864af0c7aab68491413b75740
Juan Usubiaga <juan.usubiaga@bitlogic.io>
2023-08-25 14:52:59 -0300

    add bot

--- app.js
+++ app.js
@@ -1,13 +1,123 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+const {
+  createBot,
+  createProvider,
+  createFlow,
+  addKeyword,
+} = require("@bot-whatsapp/bot");
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+const QRPortalWeb = require("@bot-whatsapp/portal");
+const BaileysProvider = require("@bot-whatsapp/provider/baileys");
+const MockAdapter = require("@bot-whatsapp/database/mock");
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+const flowSecundario = addKeyword(["2", "siguiente"]).addAnswer([
+  "📄 Aquí tenemos el flujo secundario",
+]);
+
+const flowDocs = addKeyword([
+  "doc",
+  "documentacion",
+  "documentación",
+]).addAnswer(
+  [
+    "📄 Aquí encontras las documentación recuerda que puedes mejorarla",
+    "https://bot-whatsapp.netlify.app/",
+    "\n*2* Para siguiente paso.",
+  ],
+  null,
+  null,
+  [flowSecundario]
+);
+
+const flowTuto = addKeyword(["tutorial", "tuto"]).addAnswer(
+  [
+    "🙌 Aquí encontras un ejemplo rapido",
+    "https://bot-whatsapp.netlify.app/docs/example/",
+    "\n*2* Para siguiente paso.",
+  ],
+  null,
+  null,
+  [flowSecundario]
+);
+
+const flowGracias = addKeyword(["gracias", "grac"]).addAnswer(
+  [
+    "🚀 Puedes aportar tu granito de arena a este proyecto",
+    "[*opencollective*] https://opencollective.com/bot-whatsapp",
+    "[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez",
+    "[*patreon*] https://www.patreon.com/leifermendez",
+    "\n*2* Para siguiente paso.",
+  ],
+  null,
+  null,
+  [flowSecundario]
+);
+
+const flowDiscord = addKeyword(["discord"]).addAnswer(
+  [
+    "🤪 Únete al discord",
+    "https://link.codigoencasa.com/DISCORD",
+    "\n*2* Para siguiente paso.",
+  ],
+  null,
+  null,
+  [flowSecundario]
+);
+
+const flowTalk = addKeyword([
+  "hablar",
+  "documentacion",
+  "documentación",
+]).addAnswer(
+  [
+    "📄 Aquí encontras las documentación recuerda que puedes mejorarla",
+    "https://bot-whatsapp.netlify.app/",
+    "\n*2* Para siguiente paso.",
+  ],
+  null,
+  null,
+  [flowSecundario]
+);
+
+const flowPrincipal = addKeyword(["hola", "ole", "alo"])
+  .addAnswer("🙌 Hola, bienvenido a MpazCakes, ¿Como puedo ayudarte?")
+  .addAnswer(
+    [
+      "Te comparto los siguientes links de interes.",
+      "👉 *Catalogo* https://opencollective.com/bot-whatsapp",
+      "👉 *Instagram*  https://www.instagram.com/mpazcakes",
+      "👉 Si decea hablar con asistente de local escriba *hablar*",
+    ],
+    null,
+    null,
+    [flowTalk]
+  );
+
+const main = async () => {
+  const adapterDB = new MockAdapter();
+  const adapterFlow = createFlow([flowPrincipal]);
+  const adapterProvider = createProvider(BaileysProvider);
+
+  createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB,
+  });
+
+  QRPortalWeb();
+};
+
+main();
+// app.use(express.static("public"));
+
+// app.get("*", (req, res) => {
+//   res.redirect("/");
+// });
+
+// app.listen(port, () => {
+//   console.log(`App listening on port ${port}`);
+//   main();
+// });
